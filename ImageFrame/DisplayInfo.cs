93766b7c15d9816691073787b26370767fe58eda
mggggk <mggggk@gmail.com>
2022-09-03 07:36:12 +0900

    Add basic class

---
+++ ImageFrame/DisplayInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageFrame
+{
+    public record DisplayInfo(Rectangle location, string path, Point offset, double zoom);
+}

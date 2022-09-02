545831af068e65e2bf372afed7eb1a2b54673479
mggggk <mggggk@gmail.com>
2022-09-03 07:39:03 +0900

    Add Image component in MainWindow

--- ImageFrame/MainWindow.xaml.cs
+++ ImageFrame/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,14 @@ namespace ImageFrame
             var assembly = Application.Current.MainWindow.GetType().Assembly;
             var version = FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
             Title = assembly.GetName().Name + " " + version;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var saveData = SaveDataLoader.Load();
+            if (saveData.images.Count <= 0) return;
+            if (!File.Exists(saveData.images[0].path)) return;
+            image1.Source = new BitmapImage(new Uri(saveData.images[0].path));
         }
     }
 }

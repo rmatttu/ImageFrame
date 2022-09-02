c9547170e70279930b8d7649af8ff197dcef1515
mggggk <mggggk@gmail.com>
2022-09-03 07:36:38 +0900

    Add SaveData class

---
+++ ImageFrame/SaveData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageFrame
+{
+    [Serializable()]
+    public record SaveData(List<DisplayInfo> images, string version)
+    {
+        public static SaveData GenerateSample()
+        {
+            var images = new List<DisplayInfo>();
+            images.Add(new DisplayInfo(
+                new Rectangle(10, 20, 200, 100),
+                "dummy.jpg",
+                new Point(0, 0), 1.0));
+            return new SaveData(images, "0.0.1");
+        }
+
+    }
+}
---
+++ ImageFrame/SaveDataLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ImageFrame
+{
+    public class SaveDataLoader
+    {
+        public static string Location { get; } = "SaveData.json";
+
+        public static SaveData Load()
+        {
+            if (!File.Exists(Location)) return SaveData.GenerateSample();
+            using StreamReader reader = new StreamReader(Location);
+            return JsonSerializer.Deserialize<SaveData>(reader.BaseStream);
+        }
+    }
+}
